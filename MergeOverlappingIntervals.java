e782e705cc4d95ad5f4a4f1e44f234e8976b2867
Shlomit <shlomitgazit12@hotmail.com>
2020-01-27 12:17:51 -0800

    .

--- MergeOverlappingIntervals.java
+++ MergeOverlappingIntervals.java
@@ -1,20 +1,18 @@
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Comparator;
 import java.util.Stack;
 
-import java.util.Arrays;
-import java.util.Stack;
 
 public class MergeOverlappingIntervals {
 
 	// The main function that takes a set of intervals, merges 
 	// overlapping intervals and prints the result 
-	public static void mergeIntervals(Interval arr[]) 
-	{ 
+	public static Stack<Interval>  mergeIntervals(Interval[] arr){ 
+
+    Stack<Interval> solution = new Stack<Interval>();
 		// Test if the given set has at least one interval 
 		if (arr.length <= 0) 
-			return; 
+			return solution; 
 	
 		// Create an empty stack of intervals 
 		Stack<Interval> stack=new Stack<>(); 
@@ -37,27 +35,31 @@ public class MergeOverlappingIntervals {
 			Interval top = stack.peek(); 
 	
 			// if current interval is not overlapping with stack top, push it to the stack 
-			if (top.end < arr[i].start) 
+			if (top.end < arr[i].start){
 				stack.push(arr[i]); 
-	
-			// Otherwise update the ending time of top if ending of current interval is more 
-			else if (top.end < arr[i].end) 
-			{ 
+      }
+			// Otherwise update the ending range of top if ending of current interval is higher 
+			else if (top.end < arr[i].end){ 
 				top.end = arr[i].end; 
 				stack.pop(); 
 				stack.push(top); 
 			} 
-		} 
-	
-		// Print contents of stack 
-		System.out.print("The Merged Intervals are: "); 
+    } 
+    
+    solution = stack;
+		printSolution(stack);
+    return solution;
+  } 
+
+  public static void printSolution(Stack<Interval> stack){
+    System.out.print("The Merged Intervals are: "); 
 		while (!stack.isEmpty()) 
 		{ 
       reverse(stack);
 			Interval t = stack.pop(); 
 			System.out.print("["+t.start+","+t.end+"] "); 
-		} 
-  } 
+    } 
+  }
   
   public static void reverse (Stack<Interval> s){
     
@@ -79,11 +81,12 @@ public class MergeOverlappingIntervals {
   }
 
 	public static void main(String args[]) { 
-		Interval arr[]=new Interval[3]; 
+		
+		Interval[] arr=new Interval[3]; 
 		arr[0]=new Interval(94133,94133); 
 		arr[1]=new Interval(94200,94299); 
-		arr[2]=new Interval(94600,94699); 
-		mergeIntervals(arr); 
+    arr[2]=new Interval(94600,94699); 
+		mergeIntervals(arr);
 	} 
 }
 
