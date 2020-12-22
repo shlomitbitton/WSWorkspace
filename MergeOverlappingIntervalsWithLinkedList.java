fc72a0e4c41e7c96036946c627360b992bd49e9d
Shlomit <shlomitgazit12@hotmail.com>
2020-12-22 13:08:54 -0800

    format

--- MergeOverlappingIntervalsWithLinkedList.java
+++ MergeOverlappingIntervalsWithLinkedList.java
@@ -1,45 +1,40 @@
 import java.util.Arrays;
 import java.util.LinkedList;
 
-
 public class MergeOverlappingIntervalsWithLinkedList {
 
-  public static   LinkedList<Interval> mergeIntervals(LinkedList<Interval>  intervals){
+  public static LinkedList<Interval> mergeIntervals(LinkedList<Interval> intervals) {
 
-   
-   if(intervals.size() <=1){
-     return intervals;
-   }
-   LinkedList<Interval> solution = new LinkedList<Interval>();
-   for(Interval current: intervals){
-    if(solution.isEmpty() || solution.getLast().end < current.start){ 
-      solution.add(current);
-    }else{
-      solution.getLast().end = Math.max(current.end, solution.getLast().end);
+    if (intervals.size() <= 1) {
+      return intervals;
     }
-   }
-   print(solution);
+    LinkedList<Interval> solution = new LinkedList<Interval>();
+    for (Interval current : intervals) {
+      if (solution.isEmpty() || solution.getLast().end < current.start) {
+        solution.add(current);
+      } else {
+        solution.getLast().end = Math.max(current.end, solution.getLast().end);
+      }
+    }
+    print(solution);
     return solution;
   }
 
-  public static void print(LinkedList<Interval>  intervals){
-    if(!intervals.isEmpty()){
-      System.out.print("Interval list: "); 
-      for(Interval interval:intervals){
-        System.out.println("["+ interval.start +" "+ interval.end+"],");
+  public static void print(LinkedList<Interval> intervals) {
+    if (!intervals.isEmpty()) {
+      System.out.print("Interval list: ");
+      for (Interval interval : intervals) {
+        System.out.println("[" + interval.start + " " + interval.end + "],");
       }
     }
   }
 
-public static void main(String args[]) { 
+  public static void main(String args[]) {
 
-   LinkedList<Interval> intervals = new LinkedList<Interval>(Arrays.asList(
-    new Interval(94133,94133),
-      new Interval(94200,94299),
-      new Interval(94600,94699)));
+    LinkedList<Interval> intervals = new LinkedList<Interval>(
+        Arrays.asList(new Interval(94133, 94133), new Interval(94200, 94299), new Interval(94600, 94699)));
     print(intervals);
-  mergeIntervals(intervals);
-} 
-
+    mergeIntervals(intervals);
+  }
 
 }
